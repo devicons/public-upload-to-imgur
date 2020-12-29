cde204fc239f7ad25e2d4c8dc0df0ecee9a18440
Thomas Bui <thomasbui198@gmail.com>
2020-12-29 12:01:57 -0800

    Added checks for invalid file input

--- src/index.js
+++ src/index.js
@@ -15,6 +15,11 @@ async function main() {
   const clientId = core.getInput("client_id")
   console.log("Uploading " + img_paths.length + " images...")
 
+  if (img_paths.length == 0) {
+    core.info("No image paths found. Skipping upload and exiting script.")
+    return;
+  }
+
   let links = []
   for (let img_path of img_paths) {
     uploadToImgur(img_path, description, clientId)
